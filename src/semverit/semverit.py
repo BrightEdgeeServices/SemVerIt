0692cd0f2c2e1dee97d64604bb7a122c8ad99224
Hendrik du Toit <hendrik@brightedge.co.za>
2021-10-29 16:39:08 +0200

    [DOC] Updated docstring long description

--- src/semverit/semverit.py
+++ src/semverit/semverit.py
@@ -1,8 +1,8 @@
 """Manipulate semantic versioning (SemVer)
 
-Manipulate semantic version numbers.  Currently, it only allows for the
-"0.0.0" format and should be expanded to allow for the rest of the
-specification as well.
+Manipulate semantic version numbers.  Create a new version number,
+initialize it with an, existing number or alternatively read it from an
+existing project setup.py file.
 
 See also https://semver.org/
 """
