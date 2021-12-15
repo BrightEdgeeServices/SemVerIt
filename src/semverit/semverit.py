b391b3483561177f19afa4f07af6dd36273f59d9
Hendrik du Toit <hendrik@brightedge.co.za>
2021-12-15 03:12:16 +0200

    GitHub_Issue_00014

--- src/semverit/semverit.py
+++ src/semverit/semverit.py
@@ -1,8 +1,11 @@
 """Manipulate semantic versioning (SemVer)
 
-Manipulate semantic version numbers.  Create a new version number,
-initialize it with an existing number or alternatively read it from an
-existing project setup.py file.  Compare a version number with another.
+Manipulate semantic version strings:
+1. Create a new version number,
+1. initialize it with an existing number
+1. Read it from an existing project setup.cfg file.
+1. Validate the verstion string.
+1. Compare one version numberwith another.
 
 See also https://semver.org/
 """
